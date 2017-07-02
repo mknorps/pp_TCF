1dcffaab916947731d3ab9217974e023c0a8314d
Maria Knorps <maria.knorps@gmail.com>
2017-07-02 21:36:42 +0200

    Draw figures fixed (see https://stackoverflow.com/questions/44872420/how-to-draw-and-save-a-subplot-figure-as-a-new-class-method-in-python)

--- homfigs.py
+++ homfigs.py
@@ -3,7 +3,7 @@
 # File name: homfigs.py
 # Created by: gemusia
 # Creation date: 30-06-2017
-# Last modified: 01-07-2017 21:03:06
+# Last modified: 02-07-2017 21:36:27
 # Purpose: module for creating matplotlib figures of
 #          statistics created with module Channel
 #          from 'homstat.py'
@@ -16,40 +16,42 @@ import matplotlib.pyplot as plt
 
 
 
-class Homfig(Figure):
-    
-    #tuple of possible subplot names and configurations
-    subplot_dict = [{ax1:111}, {ax1:121,ax2:122},{ax1:221,ax2:222,ax3:223},{ax1:221,ax2:222,ax3:223,ax4:224}]
+# class of figures for channel flow
+# with one subfigure
 
-    def __init__(self,n):
-        self=plt.figure()
-        for x in range(n):
-            self. = self.add_subplot(value)
+class Homfig:
+   
 
-    def get_axes_params(self):
-        return self.axes_params
+    # *args: list of plot features for ax1.plot
+    #     (xdata,ydata,str linetype,str label)
+    # **kwargs: list of axes features for ax1.set_$(STH)
+    #     possible keys:
+    #     title,xlabel,ylabel,xlim,ylim,xscale,yscale
 
-    def set_axes_params(self, **kwargs)
-        if kwargs is not None:
-            for key,value in kwargs.iteritems():
-                self.
-        title,xlabel,ylabel,xlim,xscale,yscale):
+
+    def __init__(self,*args,**kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111)
+
+        for key, val in self.kwargs.iteritems():
+            getattr(self.ax,'set_'+key)(val)
+
+    def hdraw(self):
+        self.ax.plot([0, 1, 2, 3, 4], [0, 1, 2, 3, 4], label="Test", color='g')
+        leg = self.ax.legend(loc=4)
+
+    def save(self, name="test2"):
+        self.fig.savefig(name+'.eps')
+
+ff = Homfig (title='very important title',xlabel="xxx",ylabel="yyy")
+ff.hdraw()
+ff.save()
+
 '''
-    fig2=plt.figure()
-    ax1 = fig2.add_subplot(111)  
-    ax1.set_title('St'+ x)
-    ax1.set_xlabel('$y^{+}$')
-    ax1.set_ylabel('$concentration$')
-    ax1.set_xlim([0,160])
-    ax1.set_xscale('log')
-    ax1.set_yscale('log')
-
     ax1.plot(DataFiles[cntr][:,0],DataFiles[cntr][:,1], 'k-^', label='model')
     ax1.plot(DataFiles_LES[cntr][:,0],DataFiles_LES[cntr][:,1], '-', label='LES')
     ax1.plot(DataFiles_DNS[cntr][:,0],DataFiles_DNS[cntr][:,1], '--', label='DNS')
 
-
-    leg = ax1.legend(loc=4)
-    plt.savefig(directory + 'Conc_St'+x+'.eps')
-    plt.close(fig2)
 '''
