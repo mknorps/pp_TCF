abfd6da507ed75d8d609e8773f5a83f8033b2df5
Maria Knorps <maria.knorps@gmail.com>
2017-07-28 16:12:42 +0200

    computation of tau added

--- timescales.py
+++ timescales.py
@@ -3,7 +3,7 @@
 # File name: timescales.py
 # Created by: gemusia
 # Creation date: 26-07-2017
-# Last modified: 27-07-2017 12:50:00
+# Last modified: 28-07-2017 16:11:38
 # Purpose: draw plots of time scales of turbulent channel flow
 #          DNS, a priori LES, LES and SGS
 #
@@ -14,8 +14,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import homfigs as hfig
 from os.path import expanduser
-from itertools import ifilterfalse
+from itertools import product 
 
+
+#--------------------------------------------------------------------#
+# constants 
+#--------------------------------------------------------------------#
+
+#time steps in LES and DNS
+dt_DNS = 0.005
+dt_LES = 0.02
 
 
 #--------------------------------------------------------------------#
@@ -37,8 +45,19 @@ LineStyle = {'fterm':'solid','pterm':'dashed',
         "fluid":'solid',"St1":'dashed',"St5":'dotted',"St25":'dashdot'}
 
 
-ptype = ["DNS","LES","apriori","SGSles","SGSdns"]
-Stlist = ["fluid","St0.1","St1","St5","St25"]
+#ptype = ["DNS","LES","apriori","SGSles","SGSdns"]
+#Stlist = ["fluid","St0.1","St1","St5","St25"]
+ptype = ["SGSles","SGSdns"]
+ptype_dt = {"SGSles":dt_DNS,"SGSdns":dt_DNS}
+
+Stlist = ["fluid","St1"]
+
+start_iteration = [2555,2560,2565,2570,2573,2577,2580]  #start iteration list
+
+datalines = {0:{"ls": "solid","c": "green","lbl":"y=1"},
+             2:{"ls": "dashed","c": "blue","lbl":"y=3"},
+             5:{"ls": "dotted","c": "black","lbl":"y=6"},
+             7:{"ls": "dashdot","c": "red","lbl":"y=center"}}
 
 
 
@@ -64,32 +83,59 @@ file_path = expanduser("~") + "/wyniki/time_scales/"
 pict_path = file_path
 
 
+#--------------------------------------------------------------------#
+# Data reading, symmetrisation and averaging
+# 
+#--------------------------------------------------------------------#
+
+rho = {}
+tau = {}
+
+for simulation, St in product (ptype, Stlist):
+
+
+    #++++++++++++++++++++++++++++++++++++++++++++++
+    # reading multiple files, symmetrising and 
+    # averaging the data in
+    # respective columns
+
+
+    f_iter = [symm(np.transpose(np.loadtxt(file_path + "rho_" + St + "_"  + simulation + "_" 
+                                  +str(start_iteration[0]) +".dat",skiprows=1))[1:])]
+
+    for itno in start_iteration[1:]:
+       
+        # first row of read data is iteration number, so it is ommited
+        f_iter = np.append(f_iter,[symm(np.transpose(np.loadtxt(file_path + "rho_" + St + "_" 
+            + simulation + "_" +str(itno) +".dat",skiprows=1))[1:])] , axis=0)
+
+    rho[St + "_" + simulation] =  np.mean(f_iter,axis=0)
+
+    #++++++++++++++++++++++++++++++++++++++++++++++
+    # computing relaxation time tau = min {t; rho(t)<1\e}
+
+    tau[St + "_" + simulation] = map(lambda x: ptype_dt[simulation]*next(i for i,v in enumerate(x) if v<np.exp(-1)),rho[St + "_" + simulation]) 
+    print "tau = ", tau[St + "_" + simulation]
+
+
 
 #--------------------------------------------------------------------#
-# drawing plots
+# PLOT DRAW
+#
+# we use symmetrised data
 #--------------------------------------------------------------------#
 
-corr_files = []
-corr_files_symm = []
 
-for i in range(4):
-    corr_files.append(np.transpose(np.loadtxt(file_path + "rho_Usgs_ptype" +str(i+1) +".dat",skiprows=1)))
-    # tau_files  = np.loadtxt(file_path + "Tij_" +str(i+1) +".dat")
+for simulation, St in product (ptype, Stlist):
 
+    corrfig = hfig.Homfig(title= St + ": Correlation coefficient for " +simulation + " simulation", 
+                               ylabel="$rho (t)$", xlabel="t", xlim=[0,800])
 
-    # plots of correlation coefficient
+    plotFileName = pict_path + "rho_"+ St + "_" + simulation+".eps"
 
-    corrfig = hfig.Homfig(title="Correlation coefficient for " + ptype[i], ylabel="$rho (t)$",xlabel="t",xlim=[0,600])
-    plotFileName = pict_path + "rho"+str(i+1)+".eps"
-
-
-    corrfig.add_plot(corr_files[i][1],linestyle='solid',color = 'green', label='y=1')
-    corrfig.add_plot(corr_files[i][16],linestyle='solid',color = 'red', label='y=16')
-    corrfig.add_plot(corr_files[i][3],linestyle='dotted',color = 'green', label='y=3')
-    corrfig.add_plot(corr_files[i][14],linestyle='dotted',color = 'red', label='y=14')
-    corrfig.add_plot(corr_files[i][6],linestyle='dashdot',color = 'green', label='y=6')
-    corrfig.add_plot(corr_files[i][11],linestyle='dashdot',color = 'red', label='y=11')
-    corrfig.add_plot(corr_files[i][9],linestyle='dashed', label='y=9')
+    for key,arg in zip(datalines.keys(),datalines.values()):
+        corrfig.add_plot(rho[St + "_" + simulation][int(key)],
+                         linestyle=arg["ls"],color = arg["c"], label= arg["lbl"])
 	
     corrfig.hdraw()
     corrfig.save(plotFileName)
@@ -98,44 +144,19 @@ for i in range(4):
 
 
 
-    #symmetric plots
-    corr_files_symm.append(symm(corr_files[i][1:]))
-    print corr_files_symm[i][0]
-    print corr_files_symm[i][1]
+
+# plots of correlation coefficient DNS vs a priori LES
+
+for St in Stlist:
+    corrfig = hfig.Homfig(title="Correlation coefficient of $u^{*}$ for " + St, ylabel="$rho (t)$",xlabel="t",xlim=[0,800])
+    plotFileName = pict_path + "rho_"+St+".eps"
+
     
-    corrfig_symm = hfig.Homfig(title="Correlation coefficient for " + ptype[i], ylabel="$rho (t)$",xlabel="t",xlim=[0,600])
-    plotFileName = pict_path + "rho"+str(i+1)+"_symm.eps"
+    for key,simulation in product(datalines.keys(),ptype):
+        arg = datalines[key]
+        corrfig.add_plot(rho[St + "_" + simulation][key],
+                         linestyle=arg["ls"],color = arg["c"], label= arg["lbl"])
 
-
-    corrfig_symm.add_plot(corr_files[i][0],linestyle='solid',color = 'green', label='y=1')
-    corrfig_symm.add_plot(corr_files[i][2],linestyle='dotted',color = 'green', label='y=3')
-    corrfig_symm.add_plot(corr_files[i][5],linestyle='dashdot',color = 'green', label='y=6')
-    corrfig_symm.add_plot(corr_files[i][7],linestyle='dashed', label='y=9')
-	
-    corrfig_symm.hdraw()
-    corrfig_symm.save(plotFileName)
-    print "plot created: " + plotFileName
-    plt.close(corrfig_symm.fig)
-
-
-    # plots of correlation coefficient DNS vs a priori LES
-
-for i,St in zip(range(2),Stlist):
-    corrfig = hfig.Homfig(title="Correlation coefficient for " + St, ylabel="$rho (t)$",xlabel="t",xlim=[0,600])
-    plotFileName = pict_path + "rho"+St+"DNSvsLES.eps"
-
-
-    corrfig.add_plot(corr_files[2*i][1],linestyle='solid',color = 'green', label='y=1,DNS')
-    corrfig.add_plot(corr_files[2*i][16],linestyle='solid',color = 'red', label='y=16, DNS')
-    corrfig.add_plot(corr_files[2*i+1][1],linestyle='solid',color = 'blue', label='y=1, LES')
-    corrfig.add_plot(corr_files[2*i+1][16],linestyle='solid',color = 'orange', label='y=16, LES')
-    corrfig.add_plot(corr_files[2*i][3],linestyle='dotted',color = 'green', label='y=3, DNS')
-    corrfig.add_plot(corr_files[2*i][14],linestyle='dotted',color = 'red', label='y=14, DNS')
-    corrfig.add_plot(corr_files[2*i+1][3],linestyle='dotted',color = 'blue', label='y=3, LES')
-    corrfig.add_plot(corr_files[2*i+1][14],linestyle='dotted',color = 'orange', label='y=14, LES')
-    corrfig.add_plot(corr_files[2*i][9],linestyle='dashed', color='green',label='y=9, DNS')
-    corrfig.add_plot(corr_files[2*i+1][9],linestyle='dashed',color='orange', label='y=9, LES')
-	
     corrfig.hdraw()
     corrfig.save(plotFileName)
     print "plot created: " + plotFileName
